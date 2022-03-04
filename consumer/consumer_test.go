634a5c979ec64b01ba0f8339e6cdf48e21c92f34
David Franca <davidjonasmarques@gmail.com>
2022-03-04 16:48:00 -0300

    refactor: add consumer test

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -1,12 +1,17 @@
 package consumer
 
 import (
+	"errors"
 	"github.com/Shopify/sarama"
 	"github.com/golang/mock/gomock"
 	"github.com/leroy-merlin-br/gokafka/consumer/mocks"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
+
+func addMessage(message *sarama.ConsumerMessage, c chan *sarama.ConsumerMessage) {
+	c <- message
+}
 
 func TestGetSaleOrderFromAvroRecordWithFewAttributes(t *testing.T) {
 	// Set
@@ -15,6 +20,10 @@ func TestGetSaleOrderFromAvroRecordWithFewAttributes(t *testing.T) {
 	session := mocks.NewMockConsumerGroupSession(controller)
 
 	action := func(message *sarama.ConsumerMessage) error {
+		if message.Partition != 1 {
+			return errors.New("Invalid Partition")
+		}
+
 		return nil
 	}
 
@@ -24,14 +33,25 @@ func TestGetSaleOrderFromAvroRecordWithFewAttributes(t *testing.T) {
 	}
 
 	messages := make(chan *sarama.ConsumerMessage)
-	close(messages)
+	message := &sarama.ConsumerMessage{
+		Partition: 1,
+	}
+
+	messageB := &sarama.ConsumerMessage{
+		Partition: 2,
+	}
+
+	go addMessage(message, messages)
+	go addMessage(messageB, messages)
 
 	// Expectations
-	claim.EXPECT().Messages().Return(messages).Times(1)
+	claim.EXPECT().Messages().Return(messages)
+
+	session.EXPECT().MarkMessage(message, "")
 
 	// Actions
 	error := consumer.ConsumeClaim(session, claim)
 
 	// Assertions
-	assert.Equal(t, nil, error)
+	assert.Equal(t, errors.New("Invalid Partition"), error)
 }
